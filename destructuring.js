4e23661516461dca469ed8e70bd2f0d75b53e19c
Clay O'Dell <clay.odell.music@gmail.com>
2024-02-07 13:05:14 -0600

    Object Destructuring 2 & 3, Array Destructuring 1 & 2 added

--- destructuring.js
+++ destructuring.js
@@ -4,6 +4,24 @@ let facts = {
 };
 
 let {numPlanets, yearNeptuneDiscovered} = facts;
- 
+ //Object Destructuring 1
 // console.log(numPlanets);// 8
 //console.log(yearNeptuneDiscovered);//1886
+
+//Object Destructuring 2//
+// console.log(discoveryYears);//returns {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
+//Object Destructuring 3//
+//getUserData({firstName: 'Alejandro', favoriteColor: "purple"})//returns `Your name is Alejandro and you like purple`.//
+//getuserData({firstName: 'Melissa',});//returns `Your name is Melissa and you like green`.//
+//getUserData({})//returns `Your name is undefined and you like green`.//
+
+//Array Desctructuring 1//
+//console.log(first);//returns Maya
+//console.log(last);//returns Marissa
+//console.log(third);//returns Chi
+
+//Array Desctructuring 2//
+//console.log(raindrops);//returns Raindrops on roses
+//console.log(whiskers);//returns Whiskers on kittens
+//console.log(aFewOfMyFavoriteThings);//returns ['Bright copper kettles', 'warm wool mittens', Brown paper packages tied up with strings']
