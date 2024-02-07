5bcb5627ba113d533f8308cda10023b0085fef3b
Clay O'Dell <clay.odell.music@gmail.com>
2024-02-07 10:53:41 -0600

    Object Destructuring 1 answered

---
+++ destructuring.js
@@ -0,0 +1,9 @@
+let facts = {
+    numPlanets: 8,
+    yearNeptuneDiscovered: 1886
+};
+
+let {numPlanets, yearNeptuneDiscovered} = facts;
+ 
+// console.log(numPlanets);// 8
+//console.log(yearNeptuneDiscovered);//1886
